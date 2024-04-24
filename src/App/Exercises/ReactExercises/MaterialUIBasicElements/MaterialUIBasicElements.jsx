6d913eeaaa5c0a9d57e340f606ed4c3b03aca915
Piowija <piotrwijas123@gmail.com>
2024-04-24 21:36:37 +0200

    mui

---
+++ src/App/Exercises/ReactExercises/MaterialUIBasicElements/MaterialUIBasicElements.jsx
@@ -0,0 +1,15 @@
+import { Button } from '@mui/material';
+import './styles.css';
+
+export const MaterialUIBasicElements = () => {
+  return (
+    <div>
+      <div>
+        <Button variant="contained" color="secondary">
+          Kliknij mnie
+        </Button>
+      </div>
+      <div></div>
+    </div>
+  );
+};
