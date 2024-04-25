a13a5f7edb936fb29279443adf57c28e5041252e
Piowija <piotrwijas123@gmail.com>
2024-04-25 16:10:56 +0200

    mui

---
+++ src/App/Exercises/Js/Boolean/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Exercise } from './Boolean';
+
+export const booleanRouterMeteData = {
+  path: 'boolean',
+  date: '07-09-2023',
+  linkLabel: 'Boolean ćwiczenia',
+  blockNo: 18,
+  element: <Exercise />,
+  tags: ['boolean', 'const', 'true', 'false'],
+};
