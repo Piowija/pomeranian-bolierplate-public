70e67433df3e5c20bdb7995fbb55fa9433b12327
Piowija <piotrwijas123@gmail.com>
2024-04-25 21:17:34 +0200

    mui

---
+++ src/App/Exercises/Js/JsNumbers/router-data.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Exercise } from './JsNumbers';
+
+export const jsNumbersRouterMeteData = {
+  path: 'Js-Numbers',
+  date: '07-09-2023',
+  linkLabel: 'Js numery',
+  blockNo: 18,
+  element: <Exercise />,
+  tags: ['boolean', 'const', 'true', 'false'],
+};
--- src/App/Exercises/Js/view-router-data.jsx
+++ src/App/Exercises/Js/view-router-data.jsx
@@ -4,11 +4,13 @@ import { blockRouterMetaData as blockRouterMetaDataJsVariables1 } from './Exerci
 import { blockRouterMetaData as blockRouterMetaDataJsVariables2 } from './Exercise-js-variables-2/router-data';
 import { blockRouterMetaData as blockRouterMetaDataJsFunction1 } from './Exercise-js-functions/router-data';
 import { booleanRouterMeteData } from '../Js/Boolean/router-data';
+import { jsNumbersRouterMeteData } from '../Js/JsNumbers/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaDataJsVariables1,
   blockRouterMetaDataJsVariables2,
   blockRouterMetaDataJsFunction1,
   booleanRouterMeteData,
+  jsNumbersRouterMeteData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
