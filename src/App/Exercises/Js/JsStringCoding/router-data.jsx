34df7641360f8fce30f95fedcf880d28f5da9efe
Piowija <piotrwijas123@gmail.com>
2024-04-27 19:54:02 +0200

    js-string-coding

---
+++ src/App/Exercises/Js/JsStringCoding/router-data.jsx
@@ -0,0 +1,8 @@
+import { JsStringCoding } from './JsStringCoding';
+
+export const jsStringCodingRouterMeteData = {
+  path: 'empty-values-and-comments',
+  date: '08-09-2023',
+  linkLabel: 'Puste wartości i komentarze',
+  element: <JsStringCoding />,
+};
--- src/App/Exercises/Js/view-router-data.jsx
+++ src/App/Exercises/Js/view-router-data.jsx
@@ -4,9 +4,10 @@ import { blockRouterMetaData as blockRouterMetaDataJsVariables1 } from './Exerci
 import { blockRouterMetaData as blockRouterMetaDataJsVariables2 } from './Exercise-js-variables-2/router-data';
 import { blockRouterMetaData as blockRouterMetaDataJsFunction1 } from './Exercise-js-functions/router-data';
 import { booleanRouterMeteData } from '../Js/Boolean/router-data';
-import { jsNumbersRouterMeteData } from '../Js/JsNumbers/router-data';
+import { jsNumbersRouterMeteData } from './JsNumbers/router-data';
 import { mathRouterMeteData } from './Math/router-data';
 import { jsNumbersTrainingRouterMeteData } from './JsNumbersTraining/router-data';
+import { jsStringCodingRouterMeteData } from './JsStringCoding/router-data';
 
 export const blockRouterMetaData = [
   blockRouterMetaDataJsVariables1,
@@ -16,5 +17,6 @@ export const blockRouterMetaData = [
   jsNumbersRouterMeteData,
   mathRouterMeteData,
   jsNumbersTrainingRouterMeteData,
+  jsStringCodingRouterMeteData,
 ];
 export const blockRouterData = getParsedRouterDataList(blockRouterMetaData);
